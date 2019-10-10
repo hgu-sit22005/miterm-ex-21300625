45041a874f2b8e7324cc3fa8a762290d856f9a20
21300625 <21300625@handong.edu>
2019-10-10 13:48:42 +0900

    problem01

--- Problem01/Problem01.cpp
+++ Problem01/Problem01.cpp
@@ -3,20 +3,22 @@
 #include <ctime>
 
 char* generate_3_digit()
-{
+{	
+	srand(time(0));
+	char* res = new char[10];
+	for (int i=0;i<10;i++)
+		res[i]=i+'0';
 
+	for (int i=0;i<3;i++)
+	{
+		char buf;
+		int index =rand()%(10-i);
+		buf=res[index];
+		res[0]=res[index];
+		res[index]=buf;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
+	return res;
 }
 
 int main()
@@ -38,31 +40,16 @@ int main()
 
 		std::cout << "Guess three number: (ex. 1 2 3)" << std::endl;
 		std::cin >> guess[0] >> guess[1] >> guess[2];
-		
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
+		for (int i=0;i<3;i++)
+			for (int j=0;j<3;j++)
+				if (answer[i]==guess[j])
+					if (i==j)
+						strike++;
+					else 
+						ball++;
+				else
+					out++;	
+							
 		std::cout << "S:" << strike << " B:" << ball << " O:"<< out << std::endl;
 	}
 	
